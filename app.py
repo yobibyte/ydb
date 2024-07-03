7ee80fa21641d9c1627fe94d82fa4596f18dcff1
yobibyte <vitaliykurin@gmail.com>
2024-07-03 11:14:53 +0100

    Simplify adding figures/text.

--- app.py
+++ app.py
@@ -1,63 +1,16 @@
 from flask import Flask, request, send_from_directory
 import os
-import base64
-from coolname import generate_slug
 
 YDB_DIR = ".ydb"
+ROOT_DIR = os.path.join(os.getcwd(), YDB_DIR)
 
 app = Flask(__name__)
-root_dir = os.path.join(os.getcwd(), YDB_DIR)
 
-
-def maybe_init_session_page(session_dir):
-    if not os.path.exists(session_dir):
-        os.makedirs(session_dir)
-        index_path = os.path.join(session_dir, "index.html")
-        with open(index_path, "a") as f:
-            f.write("<!DOCTYPE html>")
-            f.write("<meta http-equiv='Refresh' content='3'>")
-
-
-@app.route("/add_text", methods=["POST"])
-def add_text():
-    session_name = request.form["session_name"]
-    text = request.form["text"]
-    session_dir = os.path.join(root_dir, session_name)
-    index_path = os.path.join(session_dir, "index.html")
-
-    maybe_init_session_page(session_dir)
-
-    with open(index_path, "a") as f:
-        f.write(f"<p>{text}</p>\n")
-
-    return f"Text added to {session_dir}/index.html"
-
-
-@app.route("/add_image", methods=["POST"])
-def add_image():
-    session_name = request.form["session_name"]
-
-    image_base64 = request.form["image"]
-    image_data = base64.b64decode(image_base64)
-    image_name = generate_slug()
-    session_dir = os.path.join(root_dir, session_name)
-
-    maybe_init_session_page(session_dir)
-
-    image_path = os.path.join(session_dir, image_name)
-    with open(image_path, "wb") as img_file:
-        img_file.write(image_data)
-
-    index_path = os.path.join(session_dir, "index.html")
-    with open(index_path, "a") as f:
-        f.write(f'<p><img src="{image_name}" alt="Image"></p>\n')
-
-    return f"Picture added to {session_dir}/index.html"
 
 
 @app.route("/<path:folder>/<path:filename>")
 def serve_file(folder, filename):
-    return send_from_directory(os.path.join(root_dir, folder), filename)
+    return send_from_directory(os.path.join(ROOT_DIR, folder), filename)
 
 
 if __name__ == "__main__":
--- ydb.py
+++ ydb.py
@@ -1,31 +1,41 @@
 import requests
-import base64
 from io import BytesIO
-from coolname import generate, generate_slug
+from coolname import generate_slug
+import os
+from app import ROOT_DIR
 
 BASE_URL = "http://127.0.0.1:5000"
 SESSION_NAME = generate_slug()
 
+def maybe_init_session_page(session_dir):
+    if not os.path.exists(session_dir):
+        os.makedirs(session_dir)
+        index_path = os.path.join(session_dir, "index.html")
+        with open(index_path, "a") as f:
+            f.write("<!DOCTYPE html>")
 
 def add_text(text):
-    response = requests.post(
-        f"{BASE_URL}/add_text", data={"session_name": SESSION_NAME, "text": text}
-    )
-    return response.text
+    session_dir = os.path.join(ROOT_DIR, SESSION_NAME)
+    index_path = os.path.join(session_dir, "index.html")
+    maybe_init_session_page(session_dir)
+    with open(index_path, "a") as f:
+        f.write(f"<p>{text}</p>\n")
+    return f"Text added to {session_dir}/index.html"
 
 
 def add_figure(fig):
     buf = BytesIO()
-    fig.savefig(buf, format="png")
     buf.seek(0)
+    
+    session_dir = os.path.join(ROOT_DIR, SESSION_NAME)
+    maybe_init_session_page(session_dir)
 
-    image_base64 = base64.b64encode(buf.read()).decode("utf-8")
-    buf.close()
+    image_name = generate_slug()
+    image_path = os.path.join(session_dir, image_name)
+    fig.savefig(image_path, format="png")
 
-    url = f"{BASE_URL}/add_image"
-    data = {
-        "session_name": SESSION_NAME,
-        "image": image_base64,
-    }
-    response = requests.post(url, data=data)
-    return response.text
+    index_path = os.path.join(session_dir, "index.html")
+    with open(index_path, "a") as f:
+        f.write(f'<p><img src="{image_name}" alt="Image"></p>\n')
+
+    return f"Picture added to {session_dir}/index.html"
